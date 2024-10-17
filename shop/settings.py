6d992db36db78a3cb0732a7516fbee5b94874351
Hervey213 <herveyg604@gmail.com>
2024-10-17 13:12:02 +0000

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 # Password validation
